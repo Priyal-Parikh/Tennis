67bc5121095eda843de1bfdd068ea305e96edab1
Priyal Parikh <parikhpriyal91@yahoo.com>
2020-08-13 12:51:00 +0530

    Refactor: Initialize the game at class level rather than doing it in each test cases

--- src/test/java/com/bnpp/kata/TennisGameTest.java
+++ src/test/java/com/bnpp/kata/TennisGameTest.java
@@ -6,11 +6,11 @@ import static org.junit.Assert.assertEquals;
 
 public class TennisGameTest
 {
+    TennisGame game = new TennisGame("Serena" , "Maria");
+
     @Test
     public void testNewGameShouldReturnPlayerNames()
     {
-        TennisGame game = new TennisGame("Serena" , "Maria");
-
         assertEquals( "Serena",game.getPlayerOneName() );
         assertEquals( "Maria",game.getPlayerTwoName() );
     }
